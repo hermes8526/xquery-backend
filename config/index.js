4d9c14831df068d7e946e3160e97eeed22777191
hermes8526 <hermes8530@hotmail.com>
2022-11-17 08:20:12 -0600

    Upload Project

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const NEW_PROJECT_MSG = 'XQuery uses a cryptographic signature to verify that you are the owner of this Ethereum address. Please sign this tx to login.'
+const NODE = 'https://bsc-dataseed1.binance.org'
+const SNODE_ENDPOINT = 'http://185.182.184.238/xrs/projects'
+
+module.exports = {
+  NEW_PROJECT_MSG,
+  NODE,
+  SNODE_ENDPOINT
+}
